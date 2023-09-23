1a00978d8dd3dc181bb80792f3f5dac533a1d8c3
Mirco <mirco.cattabriga@gmail.com>
2023-09-23 15:24:32 +0200

    Fixed query for fetch

--- composables/useApiFetch.js
+++ composables/useApiFetch.js
@@ -1,6 +1,20 @@
-export const useApiFetch = (path, options) => {
-  console.log(options)
+export const useApiFetch = async (path, query) => {
+  const queryParam = query ? {
+    sort: query.sort,
+    descending: query.descending,
+    page: query.page,
+    per_page: query.per_page,
+    min_year: query.min_year,
+    max_year: query.max_year,
+    min_rating: query.min_rating,
+    max_rating: query.max_rating,
+    min_vote_count: query.min_vote_count
+  } : {}
+
   const config = useRuntimeConfig()
-  console.log(config.public.baseUrl)
-  return useFetch(path, { baseURL: config.public.baseURL, ...options })
+  const response = await useFetch(path, {
+    baseURL: config.public.baseURL,
+    query: queryParam
+  })
+  return response
 }
