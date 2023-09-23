83a7ec50c1e3bc467dd58ca18682a8ae59f9faa2
Mirco <mirco.cattabriga@gmail.com>
2023-09-23 15:24:49 +0200

    Fixed movie methods

--- stores/movie.js
+++ stores/movie.js
@@ -79,31 +79,41 @@ export const useMovieStore = defineStore({
   actions: {
     async loadMovies (payload) {
       const genres = this.filters.genres.length > 0 ? this.filters.genres.join('|') : ''
-      try {
-        const res = await useFetch('http://localhost:3000/api/v1/movies/' +
-          '?sort=' + this.filters.sortString +
-          '&descending=' + payload.pagination.descending +
-          '&page=' + payload.pagination.page +
-          '&per_page=' + payload.pagination.rowsPerPage +
-          '&min_year=' + this.filters.yearMin +
-          '&max_year=' + this.filters.yearMax +
-          '&min_rating=' + this.filters.ratingMin +
-          '&max_rating=' + this.filters.ratingMax +
-          '&min_vote_count=' + this.filters.voteCountMin +
-          '&genres=' + genres
 
-        )
-        this.pagination = payload.pagination
-        this.movies = res.data.value.results
-        this.pagination.rowsNumber = res.data.total_results
-      } catch (error) {
-        showNotification({ message: error.message, type: 'error' })
-      }
+      const { data, error, pending } = await useApiFetch('/movies',
+        {
+          sort: this.filters.sortString,
+          descending: payload.pagination.descending,
+          page: payload.pagination.page,
+          per_page: payload.pagination.rowsPerPage,
+          min_year: this.filters.yearMin,
+          max_year: this.filters.yearMax,
+          min_rating: this.filters.ratingMin,
+          max_rating: this.filters.ratingMax,
+          genres: genres
+        }
+        // '?sort=' + this.filters.sortString +
+        // '&descending=' + payload.pagination.descending +
+        // '&page=' + payload.pagination.page +
+        // '&per_page=' + payload.pagination.rowsPerPage +
+        // '&min_year=' + this.filters.yearMin +
+        // '&max_year=' + this.filters.yearMax +
+        // '&min_rating=' + this.filters.ratingMin +
+        // '&max_rating=' + this.filters.ratingMax +
+        // '&min_vote_count=' + this.filters.voteCountMin +
+        // '&genres=' + genres
+
+      )
+      this.pagination = await payload.pagination
+      console.log(data.value.results)
+      this.movies = await data.value.results
+      this.pagination.rowsNumber = await data.total_results
+
     },
     async loadGenres () {
       try {
-        const res = await useFetch('http://localhost:300/movie_genres')
-        this.genres = res.data.value
+        const { data, error, pending } = await useApiFetch("movie_genres")
+        this.genres = await data.value
       } catch (error) {
         showNotification({ message: error.message, type: 'error' })
       }
@@ -122,7 +132,7 @@ export const useMovieStore = defineStore({
         const { data, error, pending } = await useApiFetch("movies/" + id)
         this.currentMovie = data.value.data.attributes
       } catch (error) {
-        showNotification({ message: error.message, type: 'error' })
+        // showNotification({ message: error.message, type: 'error' })
       }
     },
     async setWatchedMovie (payload) {
@@ -179,20 +189,37 @@ export const useMovieStore = defineStore({
 
     },
     async loadSimilarMovies (id) {
-      const { data, pending, error, refresh } = await useApiFetch('movies/' + id + '/similar_movies')
-      this.loadingSimilarMovies = pending
-      this.similarMovies = data.value.data
+      try {
+        const { data, pending, error, refresh } = await useApiFetch('movies/' + id + '/similar_movies')
+        this.loadingSimilarMovies = pending
+        this.similarMovies = data.value.data
+      } catch (error) {
+        showNotification({ message: error.message, type: 'error' })
+
+      }
+
+
     },
     async loadCuriousFacts (id) {
-      const { data, pending, error, refresh } = await useApiFetch('movies/' + id + '/curious_facts')
-      this.loadingFacts = pending
-      this.curiousFacts = data.value.data
+      try {
+        const { data, pending, error, refresh } = await useApiFetch('movies/' + id + '/curious_facts')
+        this.loadingFacts = pending
+        this.curiousFacts = data.value.data
+      } catch (error) {
+        showNotification({ message: error.message, type: 'error' })
+
+      }
 
     },
     async loadQuotes (id) {
-      const { data, pending, error, refresh } = await useApiFetch('movies/' + id + '/quotes')
-      this.loadingQuotes = pending
-      this.quotes = data
+      try {
+        const { data, pending, error, refresh } = await useApiFetch('movies/' + id + '/quotes')
+        this.loadingQuotes = pending
+        this.quotes = data
+      } catch (error) {
+        showNotification({ message: error.message, type: 'error' })
+
+      }
 
     },
     async loadCredits (id) {
@@ -215,12 +242,18 @@ export const useMovieStore = defineStore({
 
     },
     async loadContentRating (id) {
-      const { data, pending, error, refresh } = await useApiFetch('movies/' + id + '/content_rating')
-      this.loadingRatingTopics = pending
-      this.ratingTopics = data.value.rating_topics
-      this.currentMovie.content_rating_info = data.value.content_rating_info
-      this.currentMovie.content_rating = data.value.content_rating
-      this.currentMovie.suggested_audience = data.value.content_rating
+      try {
+
+        const { data, pending, error, refresh } = await useApiFetch('movies/' + id + '/content_rating')
+        this.loadingRatingTopics = pending
+        this.ratingTopics = data.value.rating_topics
+        this.currentMovie.content_rating_info = data.value.content_rating_info
+        this.currentMovie.content_rating = data.value.content_rating
+        this.currentMovie.suggested_audience = data.value.content_rating
+      } catch (error) {
+        showNotification({ message: error.message, type: 'error' })
+
+      }
 
     },
     async loadUserMovies (payload) {
@@ -253,7 +286,7 @@ export const useMovieStore = defineStore({
     async loadMovieRanks (payload) {
       payload = { pagination: {} }
       const { data, pending, error, refresh } = await useApiFetch('movie_ranks')
-      this.movieRanks = data.value.data
+      this.movieRanks = await data.value.data
     },
     async loadMovieRank (id) {
       try {
